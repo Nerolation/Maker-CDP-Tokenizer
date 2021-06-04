0cea27846c99ee0e692337fdb32a2dbbb78d3bf9
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2021-06-04 14:47:26 +0200

    Update firstAttempts.sol

--- firstAttempts.sol
+++ firstAttempts.sol
@@ -4,6 +4,124 @@ pragma solidity ^0.8.0;
 
 
 import "CDPToken.sol";
+import "inheritingForTestPurpose.sol";
+
+
+
+contract CDPTokenizer is inheritingForTestingPurpose{
+     
+    address private owner;
+    mapping(address => uint256) tokenizedCDPs;  // Keep track of senders` CDP IDs
+    mapping(uint256 => uint256) tokenization;   // Keep track of swap ratios
+    
+    CDPToken cdp_token;                         // Token Contract
+    
+    // Maker Dao 
+    DssCdpManagerLike private mk_cdpman  = DssCdpManagerLike(0x5ef30b9986345249bc32d8928B7ee64DE9435E39);
+    ProxyRegistryLike private mk_proxy   = ProxyRegistryLike(0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4);
+    
+    
+    // Only Owner
+    modifier onlyOwner {
+        require(msg.sender==owner, "Not the owner");
+        _;
+    }
+    
+    constructor() {
+         owner = msg.sender;
+     }
+     
+    /**
+     * @dev Create CDPToken Contract
+     * 
+     * The contract required a `CdpTokenizer` to be defined
+     * The `CdpTokenizer` is set to the address of this contract to
+     * permit others minting and burning tokens
+     */
+    function createTokenContract() public onlyOwner returns(address) {
+         cdp_token = new CDPToken(address(this));
+         return address(cdp_token);
+    }
+    
+    /**
+     * @dev Update Tokenizer within the Token Contract
+     * 
+     * This function allows to flexibly upgrade this Contract
+     * Execute this function after deploying the upgraded contract
+     */
+    function updateCDPTokenizer(address newtokenizer) public onlyOwner {
+         require(cdp_token.setTokenizer(newtokenizer), "Switch failed");
+    }
+    
+    /**
+     * @dev Tokenize CDP by transfering ownership and reciving tokens.
+     * 
+     * Automatically takes the `sender` first CDP 
+     */
+    function tokenizeCDP() public {
+        // Get CDP ID
+        uint CDPID = mk_cdpman.getFirstCDP(mk_proxy.proxies(msg.sender));
+        
+        mk_cdpman.give(CDPID, address(this));
+        
+        // Mint
+        cdp_token.mint(msg.sender, 1 ether);
+        
+        // Store swap ratio
+        tokenization[CDPID] = 1 ether;
+        
+        // Store address of locking entity
+        tokenizedCDPs[msg.sender] = CDPID;
+    }
+    
+    /**
+     * @dev Overloaded function, see above.
+     * 
+     * Allows to specify a specific CDP by ID
+     */
+    function tokenizeCDP(uint256 CDPID) public {
+         mk_cdpman.give(CDPID, address(this));
+         cdp_token.mint(msg.sender, 1 ether);
+         tokenization[CDPID] = 1 ether;
+         tokenizedCDPs[msg.sender] = CDPID;
+    }
+    
+    /**
+     * @dev Overloaded function, see above.
+     * 
+     * Allows to split one's CDP in as many tokens as desired
+     */
+    function tokenizeCDP(uint256 CDPID,uint256 preferedNumber) public {
+         mk_cdpman.give(CDPID, address(this));
+         cdp_token.mint(msg.sender, preferedNumber);
+         tokenization[CDPID] = preferedNumber;
+         tokenizedCDPs[msg.sender] = CDPID;
+    }
+    
+    /**
+     * @dev Unlock CDP without any arguments 
+     */
+    function unlockCDP() public {
+        // Get CDP ID
+        uint CDPID = tokenizedCDPs[msg.sender];
+        
+        // Make sure that all minted tokens are returned
+        require(tokenization[CDPID] == cdp_token.balanceOf(msg.sender), "Not all tokens provided");
+
+        // Burn
+        cdp_token.burn(msg.sender, tokenization[CDPID]);
+        
+        // Grant access right of CDP to sender
+        mk_cdpman.give(CDPID, msg.sender);
+    }
+    
+    
+    
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
 
 // Proxy Registry Interface
 interface ProxyRegistryLike {
@@ -13,14 +131,27 @@ interface ProxyRegistryLike {
     function build(address owner) external;
 }
 
+
+
+
 // CDP Interface
-contract DssCdpManagerLike {
-    mapping(address => uint256) public first; // Owner => First CDPId
+// NEEDS TO BE PLACED INTO THE MAIN FAIL AFTER REMOVING TESTS
+
+
+interface DssCdpManagerLike {
+    function getFirstCDP(address addr) external view returns (uint);
+    function getOwner(uint) external view returns (address);
+    function give(uint cdp, address dst) external;
 }
 
-contract CDPTokenizer {
-     
-    address private _owner;
+
+
+
+
+
+
+contract inheritingForTestPurpose{
+    
     ProxyRegistryLike private mk_proxy   = ProxyRegistryLike(0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4);
     address private mk_action  = 0x82ecD135Dce65Fbc6DbdD0e4237E0AF93FFD5038;
     DssCdpManagerLike private mk_cdpman  = DssCdpManagerLike(0x5ef30b9986345249bc32d8928B7ee64DE9435E39);
@@ -29,31 +160,8 @@ contract CDPTokenizer {
     address private mk_daijoin = 0x9759A6Ac90977b93B58547b4A71c78317f391A28;
     bytes32 ilk =
         0x4554482d41000000000000000000000000000000000000000000000000000000;
-        
-    CDPToken _tcdp;
-    
     uint256 public cdpi;
-
-     
-     
-     
-     
-    constructor() {
-         _owner = msg.sender;
-     }
-     
-     
-    function createTokenContract() public returns(address) {
-         _tcdp = new CDPToken(address(this));
-         return address(_tcdp);
-    }
-     
-    function updateCDPTokenizer(address newtokenizer) public {
-         require(_tcdp.setTokenizer(newtokenizer), "Switch failed");
-    }
-     
-     
-     function buildProxyOpenCdpLockETH() public payable {
+    function buildProxyOpenCdpLockETH() public payable {
         if (mk_proxy.proxies(msg.sender) == address(0)) {
             mk_proxy.build(msg.sender);
             
@@ -77,14 +185,7 @@ contract CDPTokenizer {
                                                                                                                     payload
                                                                                                                     ));
         require(success, "no success");
-        cdpi = mk_cdpman.first(mk_proxy.proxies(msg.sender));
+        cdpi = mk_cdpman.getFirstCDP(mk_proxy.proxies(msg.sender));
      }
-    
-    
-    
-    
-    
-    
-    
     
 }
